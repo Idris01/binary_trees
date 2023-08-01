58d3667a28098764fa8dc54a72ded61f90519342
Idris Adebowale <idrys01@gmail.com>
2023-08-01 07:36:37 +0000

    add function that delete a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Delete a binary tree
+ * @tree: pointer to root of binary tree
+ *
+ * Return: always void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
